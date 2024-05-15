d2fa045e0142b2c54d11c1a3bc1714d17f71176a
pipwitheridge <phillipwitheridge@gmail.com>
2024-05-15 23:07:46 +1000

    Trying AGAIN to implement GA tracking

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import Home from './Components/Home';
 import { React, Container } from "react-bootstrap";
 import Navbar1 from "./Components/Navbar/Navbar";
 import TindelHome from "./Components/TindelHome";
-import ReactGA from 'react-ga';
-import useGATracker from "./useGATracker";
+import ReactGA from "react-ga4";
 
 
 function App() {
 
-  // GA Tracking setup
+  // GA Tracking setup OLD
+  /* 
   const TRACKING_ID = "G-7XD03E57PD"; // OUR_TRACKING_ID
   ReactGA.initialize(TRACKING_ID);
   useEffect(() => {
     useGATracker();
-  }, []);
+  }, []); */
+
+  // GA New
+
+  ReactGA.initialize("G-7XD03E57PD");
+  ReactGA.send({ hitType: "pageview", page: "/tindel-web", title: "Tindel" });
+
 
   return (
 
